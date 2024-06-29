7c1b3d524b5c9c613ca41e8c39188621833f9c6f
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-29 16:19:56 -0400

    GET /workouts
    1. Add array of workout data
    2. In app.js, add route `/workouts`
    3. In workoutsController.js implement route to get all workouts

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 
 const app = express();
 
+// Controllers
+const workoutsController = require("./controllers/workoutsController")
+
+app.use("/workouts", workoutsController)
+
 // Health Check Route
 app.get("/", (req, res) => {
     res.send("Welcome to my fitness app!")
